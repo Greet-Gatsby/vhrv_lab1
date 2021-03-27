515c928851b45a7bd75aacc254e74e552e626ebb
Greet-Gatsby <58636754+Greet-Gatsby@users.noreply.github.com>
2021-03-27 16:28:40 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -54,7 +54,7 @@ dp.add_error_handler(error)
 updater.start_webhook(listen="0.0.0.0",
 port=int(PORT),
 url_path=TOKEN)
-updater.bot.setWebhook('https://vhrvlab1.herokuapp.com/' + TOKEN)
+updater.bot.setWebhook('https://git.heroku.com/vhrvlab1.git' + TOKEN)
 
 updater.idle()
 
